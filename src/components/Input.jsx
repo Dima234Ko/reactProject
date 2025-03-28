a1a3423929d939398b7c94027f4a8bb8b054d115
esipov@sv-en.ru <esipov@sv-en.ru>
2025-03-28 10:49:33 +0300

    добавлен глаз в инпуты с типом пароль

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 export const Input = ({
   id,
   type,
@@ -6,25 +8,64 @@ export const Input = ({
   onChange,
   disabled,
   onBlur,
-  onKeyDown, 
+  onKeyDown,
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const handleChange = (event) => {
     if (!disabled) {
       onChange(event);
     }
   };
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <input
-      className="some-input"
-      id={id}
-      type={type}
-      placeholder={placeholder}
-      value={value || ""}
-      onChange={handleChange}
-      onBlur={onBlur}
-      onKeyDown={onKeyDown}
-      disabled={disabled}
-    />
+    <>
+      {type === 'password' ? (
+        <div className="input-content">
+          <input
+            className="some-input"
+            id={id}
+            type={showPassword ? 'text' : 'password'}
+            placeholder={placeholder}
+            value={value || ""}
+            onChange={handleChange}
+            onBlur={onBlur}
+            onKeyDown={onKeyDown}
+            disabled={disabled}
+          />
+          <button
+            type="button"
+            onClick={togglePasswordVisibility}
+            disabled={disabled}
+            style={{
+              marginLeft: '-30px',
+              marginTop: '18px',
+              border: 'none',
+              background: 'transparent',
+              padding: 6,
+              cursor: disabled ? 'not-allowed' : 'pointer',
+            }}
+          >
+            {showPassword ? '👁️‍🗨️' : '👁️'}
+          </button>
+        </div>
+      ) : (
+        <input
+          className="some-input"
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          value={value || ""}
+          onChange={handleChange}
+          onBlur={onBlur}
+          onKeyDown={onKeyDown}
+          disabled={disabled}
+        />
+      )}
+    </>
   );
 };
